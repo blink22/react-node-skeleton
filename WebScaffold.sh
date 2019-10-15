6a6d05dafbc4b7b819a20b796fa012bd8fd0fb26
AshrafHofny <ashraf.hofny@blink22.com>
2019-10-15 21:17:19 +0200

    check folder existence first

--- WebScaffold.sh
+++ WebScaffold.sh
@@ -2,25 +2,31 @@
 echo "Welcome"
 read -p "Please enter your Project Name  : " name
 echo "Nice"
+if [ ! -d "../$name" ]
+then
+    echo "File doesn't exist. Creating now"
+    mkdir ./$name
+    mkdir ../$name
+    mkdir ../$name/"client"
+    mkdir ../$name/"server"
 
-mkdir ../$name
-mkdir ../$name/"client"
-mkdir ../$name/"server"
+    echo "Copying FrontEnd files"
+    cp -R Data/client/* ../$name/"client"
 
-echo "Copying FrontEnd files"
-cp -R Data/client/* ../$name/"client"
+    echo "Copying BackEnd files"
+    cp -R Data/server/* ../$name/"server"
 
-echo "Copying BackEnd files"
-cp -R Data/server/* ../$name/"server"
+    echo "Preparing FE environment"
+    cd ../$name/"client"
+    npm install
 
-echo "Preparing FE environment"
-cd ../$name/"client"
-npm install
+    echo "Preparing BE environment"
+    cd ../"server"
+    npm install
 
-echo "Preparing BE environment"
-cd ../"server"
-npm install
-
-echo "Project is ready to use"
-echo "Find FE Project => client"
-echo "Find BE Project => server"
+    echo "Project is ready to use"
+    echo "Find FE Project => client"
+    echo "Find BE Project => server"
+else
+    echo "Folder exists"
+fi
